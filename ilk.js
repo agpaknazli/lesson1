1773d6d92afd967f2b398222ff1b57bdb9a9a07f
agpaknazli <agpaknazli19@gmail.com>
2024-03-31 12:16:05 +0200

    first commit

---
+++ ilk.js
@@ -0,0 +1,72 @@
+console.log("intro js içindeyiz")
+// console.error("Hata Mesajı")
+console.warn("uyarı mesajı")
+document.write("javascprit intro")
+cevap=confirm("çıkmak istediğinize emin misiniz")
+console.log(cevap)
+
+x=15
+ad="Helen"
+yas=40
+
+console.log(ad)
+
+let toplam=0
+x=60
+y=70
+toplam=x+y
+console.log(toplam)
+
+var sayi=5
+
+
+
+{
+//burası local alan
+console.log(sayi)
+
+const pi=3.14
+
+}
+
+//console.log(pi)
+
+
+var number =90
+{
+
+    console.log(number)
+    var number=40
+    
+}
+
+console.log(number+15)
+
+
+yas=40
+sayi=56.87
+console.log(yas)
+sonuc=(sayi*5)+6
+console.log(sonuc)
+
+const pi= Math.PI
+console.log(pi)
+
+let yuvarlanmissonuc=Math.round(sonuc)
+console.log("sonuc", sonuc)
+console.log(yuvarlanmissonuc)
+
+let truncsonuc=Math.trunc(sonuc)
+console.log(truncsonuc)
+
+let fixedsonuc= sonuc.toFixed(2)
+console.log(sonuc.toFixed(2))
+console.log(fixedsonuc)
+
+console.clear
+console.log(Math.round(Math.random()*100))
+
+console.log(Math.pow(5,3))
+
+console.log(Math.sqrt(144))
+
